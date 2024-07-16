bc016444a34e3b4b9621e5007495a54370cc453f
sivayarramsetti <sivayarramsetti39@gmail.com>
2024-07-16 18:31:32 +0530

    mi mobiles code done

---
+++ src/main/java/mi/NewDBDatabaseConnector.java
@@ -0,0 +1,28 @@
+package mi;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import schemacrawler.tools.databaseconnector.DatabaseConnector;
+import schemacrawler.tools.databaseconnector.DatabaseServerType;
+
+public final class NewDBDatabaseConnector
+  extends DatabaseConnector
+{
+
+  private static final Logger LOGGER = Logger
+    .getLogger(NewDBDatabaseConnector.class.getName());
+
+  public NewDBDatabaseConnector()
+  {
+    super(new DatabaseServerType("newdb", "NewDB"),
+          "/help/Connections.newdb.txt",
+          "/schemacrawler-newdb.config.properties",
+          "/newdb.information_schema",
+          "jdbc:newdb:.*");
+    // SchemaCrawler will control output of log messages if you use JDK logging
+    LOGGER.log(Level.INFO, "Loaded plugin for NewDB");
+  }
+  
+}
